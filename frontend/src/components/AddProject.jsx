9eee341ca5639b26ead481bf1524ec26926b5686
Dana Popa <popa.dana100@gmail.com>
2024-05-14 13:33:30 +0300

    fullstack app with symfony and react

--- frontend/src/components/AddProject.jsx
+++ frontend/src/components/AddProject.jsx
@@ -3,39 +3,41 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
 const AddProject = () => {
-  const [name, setName] = useState('');
-  const [description, setDescription] = useState('');
-  const navigate = useNavigate(); // Create an instance of useHistory
+    const [name, setName] = useState('');
+    const [description, setDescription] = useState('');
+    const navigate = useNavigate(); // Create an instance of useHistory
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    axios
-      .post('http://localhost:8007/api/projects', { name, description })
-      .then((response) => {
-        console.log('Success:', response.data);
-        navigate('/projects'); // Navigate to the project list page
-      })
-      .catch((error) => console.error('Error:', error));
-  };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        axios
+            .post('http://localhost:8007/api/projects', { name, description })
+            .then((response) => {
+                console.log('Success:', response.data);
+                navigate('/projects'); // Navigate to the project list page
+            })
+            .catch((error) => console.error('Error:', error));
+    };
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <h1>Add Project</h1>
-      <input
-        type="text"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-        placeholder="Project Name"
-      />
-      <input
-        type="text"
-        value={description}
-        onChange={(e) => setDescription(e.target.value)}
-        placeholder="Project Description"
-      />
-      <button type="submit">Add Project</button>
-    </form>
-  );
+    return (
+        <form onSubmit={handleSubmit}>
+            <h1 className='myStyle'>Add Project</h1>
+            <input
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                placeholder="Project Name"
+            />
+            <br></br>
+            <input
+                type="text"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+                placeholder="Project Description"
+            />
+            <br></br>
+            <button type="submit">Add Project</button>
+        </form>
+    );
 };
 
 export default AddProject;
